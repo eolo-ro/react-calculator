da484bcfdcd5759786201b7a93805eec43aeb67e
eolo-ro <benjamincorderog@gmail.com>
2023-03-18 00:47:24 -0300

    added css to the html elements

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">1234</div>
+        <div className="current-operand">1234</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
